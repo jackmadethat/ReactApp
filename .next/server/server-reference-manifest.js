21b5a690083811de976948c9606afab351eb8273
jackmadethat <underscore@gmx.com>
2024-08-01 07:32:16 +0930

    Initial upload

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"iexEByX3Pn/BOLq5LAWJndHY3vU1TziTiVjlt9KpLG0=\"}"
---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/ReactApp",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
+  
